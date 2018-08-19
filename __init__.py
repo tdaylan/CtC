5dae094bed535d2f78e37a80ac93a93c24641860
Tansu Daylan <tansu.daylan@gmail.com>
2018-08-19 18:00:17 -0400

    Modified the guideline

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 # general guidelines
 ## please accompy code with comments 
 ## commit frequently
+    ### maintain separate branches for major tasks, which will then be merged into the master branch
     ### allows faster communication among contributors to the code
     ### reduces the chances of losing work in the local repository
 ## try to maintain the namespace conventions
