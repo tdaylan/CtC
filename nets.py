6d0ea75a454cf140f1decf751b935c0b163f625f
Jeremy Dudo <jmdudo2@gmail.com>
2018-09-01 01:20:23 -0400

    Got most of the methods cleaned
    Methods separated, prfm == 'performance function' which can be called to give the performance of whichever full model that is constructed
    
    I want to change the modl methods a bit so that they can be called from the script to specify a series of layers at any point in time that can be changed in a less hard-coded way

--- nets.py
+++ nets.py
@@ -93,7 +93,10 @@ class nets():
         self.inpttran = inpt[numbdatatest:, :]
         self.outptran = outp[numbdatatest:, :]
 
-    def Sqnl(self):
+    def fcon(self):
+        """
+        Initialize Fully Connected model
+        """
         modl = Sequential()
         modl.add(Dense(self.numbdimsfrst, input_dim=self.numbtime, activation='relu'))
         modl.add(Dropout(self.fracdropfrst))
@@ -102,9 +105,30 @@ class nets():
         modl.add(Dense(1, activation='sigmoid'))
         modl.compile(loss='binary_crossentropy', optimizer='remsprop', metrics=['accuracy'])
 
+        return modl
+
+    def Cnn1D(self):
+        """
+        Initialize Convolutional 1D Neural Network model
+        """
+        modl = Sequential()
+        modl.add(Conv1D(self.numbdimsfrst, self.numbdimsfrst input_shape=(self.numbdimsfrst[1], 1), activation='relu')) # gotta fix this
+        modl.add(Dropout(self.fracdropfrst))
+        modl.add(Conv1D(self.numbdimsseco, self.numbdimsseco activation='relu'))
+        modl.add(Dropout(self.fracdropseco))
+        modl.add(Dense(1, activation='sigmoid'))
+        modl.compile(loss='binary_crossentropy', optimizer='remsprop', metrics=['accuracy'])
+
+        return modl
+
+    def prfm(self, model):
+        """
+        Performance method
+        """
         metr = np.zeros((self.gdat.numbepoc, 2, 3)) - 1
         loss = np.empty(self.gdat.numbepoc)
-        numbepocchec = 5
+        numbepocchec = 5 # hard coded
+        modl = model
 
         for y in self.gdat.inxepoc:
             hist = modl.fit(self.inpt, self.outp, epochs=1, batch_size=self.numbdatabtch, validation_split=self.gdat.fractest, verbose=0)
@@ -135,56 +159,6 @@ class nets():
                 flne = matrconf[1, 0]
                 trpo = matrconf[1, 1]
 
-
-                if float(trpo + flpo) > 0:
-                    metr[y, r, 0] = trpo / float(trpo + flpo)
-                metr[y, r, 1] = float(trpo + trne) / (trpo + flpo + trne + flne)
-                if float(trpo + flne) > 0:
-                    metr[y, r, 2] = trpo / float(trpo + flne)
-            return metr
-
-    def Cnn1D(self):
-        modl = Sequential()
-        modl.add(Conv1D(self.numbdimsfrst, input_dim=self.numbtime, activation='relu'))
-        modl.add(Dropout(self.fracdropfrst))
-        modl.add(Conv1D(self.numbdimsseco, activation='relu'))
-        modl.add(Dropout(fracdropseco))
-        modl.add(Dense(1, activation='sigmoid'))
-        modl.compile(loss='binary_crossentropy', optimizer='remsprop', metrics=['accuracy'])
-
-        metr = np.zeros((self.gdat.numbepoc, 2, 3)) - 1
-        loss = np.empty(self.gdat.numbepoc)
-        numbepocchec = 5
-
-        for y in self.gdat.inxepoc:
-            hist = modl.fit(self.inpt, self.outp, epochs=1, batch_size=self.numbdatabtch, validation_split=self.gdat.fractest, verbose=0)
-            loss[y] = hist.history['loss'][0]
-            inxepocloww = max(0, y- numbepocchec)
-            if y == self.gdat.numbepoc - 1 and 100. * (loss[indxepocloww] - loss[y]):
-                print('Warning! The optimizer may not have converged.')
-                print('loss[indxepocloww]\n', loss[indxepocloww], '\nloss[y]\n', loss[y], '\nloss\n', loss)
-
-            for r in self.gdat.inxrtyp:
-                if r==0:
-                    inpt = self.inpttran
-                    outp = self.outptran
-                    numdatatemp = self.numbdatatran
-                else:
-                    inpt = self.inpttest
-                    outp = self.outptest
-                    numbdatatemp = self.numbdatatest
-
-                outppred = (modl.predict(inpt) > 0.5).astype(int)
-                score = modl.evaluate(inpt,outp, verpose=0)
-                matrconf = confusion_matrix(outp[:, 0], outppred[:, 0])
-
-
-
-                trne = matrconf[0, 0]
-                flpo = matrconf[0, 1]
-                flne = matrconf[1, 0]
-                trpo = matrconf[1, 1]
-                
 
                 if float(trpo + flpo) > 0:
                     metr[y, r, 0] = trpo / float(trpo + flpo)
